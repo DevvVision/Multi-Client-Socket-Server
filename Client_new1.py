cc74576f6605c6979c30836f1a126937e305acca
Harshit Srivastava <167855220+DevvVision@users.noreply.github.com>
2024-06-07 08:30:38 +0530

    Add files via upload

---
+++ Client_new1.py
@@ -0,0 +1,33 @@
+import socket
+import os
+import subprocess
+import pyautogui
+import threading
+s=socket.socket()
+host="192.168.1.34"
+port=9999
+s.connect((host,port))
+def sendMsg():
+	try:
+		send = input(">>")
+		s.send(send.encode("utf-8"))
+	except Exception as e:
+		sendMsg()
+def recving():
+	while True:
+		try:
+			dt = str(s.recv(1024),'utf-8')
+			print(dt)
+		except Exception as e:
+			recving()
+thr1 = threading.Thread(target=recving)
+thr1.start()
+# thr2 = threading.Thread(target=sendMsg)
+# thr2.start()
+name = input("Enter your name : ")
+s.send(name.encode('utf-8'))
+while True:
+	sendMsg()
+
+
+	
